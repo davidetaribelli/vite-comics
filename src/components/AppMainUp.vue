481299cd64a3cd275bff26c8716e76e5968078cc
davidetaribelli <davide.taribelli@gmail.com>
2023-05-22 15:52:46 +0200

    PROPS

--- src/components/AppMainUp.vue
+++ src/components/AppMainUp.vue
@@ -1,6 +1,14 @@
 <script>
 export default {
     name: "AppMainUp",
+    props: [
+        "cards"
+    ],
+    data() {
+        return {
+
+        }
+    }
 }
 </script>
 
@@ -14,13 +22,19 @@ export default {
 
     <div class="container">
         <div class="row">
-            <div class="col-12 bg-dark d-flex align-items-center">
+            <div class="col-12 bg-dark d-flex align-items-center p-4">
                 <div class="currentSeries text-white p-2">
                     <span> CURRENT SERIES</span>
                 </div>
-                <div id="contentHere" class="text-white">
-
+                <div id="contentHere" class="text-white d-flex flex-wrap justify-content-between align-items-center p-3">
+                    <div v-for="card in cards" class="card_">
+                        <img :src="card.thumb" alt="card.type">
+                        <small>{{ card.series }}</small>
+                    </div>
                 </div>
+            </div>
+            <div class="load_ text-center">
+                <button class="py-1 px-4 border-0 m-4 text-white"> LOAD MORE </button>
             </div>
         </div>
     </div>
@@ -43,17 +57,30 @@ export default {
 
 .col-12 {
     position: relative;
-    height: 5rem;
-}
-
-#contentHere {
-    height: 100%;
 }
 
 .currentSeries {
     position: absolute;
     top: -1.4rem;
     font-weight: 700;
+    background-color: #0282f9;
+}
+
+.card_ {
+    width: calc(100vw / 8 - 2em);
+    height: calc(100vw / 8 - 2em);
+    margin: 1em
+}
+
+.card_ img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+}
+
+.load_>button {
+    cursor: pointer;
     background-color: #0282f9;
 }
 </style>
